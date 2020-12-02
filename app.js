0abf0925389dbf22e4fc0692b7af156554b6b042
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-02 10:29:06 +0100

    oui

---
+++ app.js
@@ -0,0 +1,10 @@
+function changement(id, texte){
+    document.getElementById(id).innerHTML = texte;
+}
+
+changement("p1", "salut");
+changement("p2", "le");
+changement("p3", "texte");
+changement("p4", "change");
+changement("p5", "c'est cool");
+
